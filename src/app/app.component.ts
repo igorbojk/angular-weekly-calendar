369b78e7191f1e91b14cf0280d4f180935094648
Igor <igorbojk@gmail.com>
2018-10-08 17:11:51 +0300

    stage save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   public hoursArray: any = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
@@ -44,5 +44,42 @@ export class AppComponent {
       number: 14
     }
   ];
+
+  public daysNames: any = [
+    'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'
+  ];
+
+  public month: any = [
+    8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+  ];
+
+  public numberOfWeek: any = 1;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.generateWeek();
+  }
+
+
+  generateWeek() {
+    this.week = [];
+    this.month.slice((7 * this.numberOfWeek) - 7, 7 * this.numberOfWeek).forEach((elem, index) => {
+      this.week.push({day: this.daysNames[index], number: elem});
+    });
+    console.log(this.week);
+  }
+
+  nextWeek() {
+    this.numberOfWeek++;
+    this.generateWeek();
+  }
+
+  prevWeek() {
+    this.numberOfWeek--;
+    this.generateWeek();
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import {MatCardModule, MatTabsModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatTabsModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
@@ -14,10 +14,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatButtonModule
   ],
   exports: [
-    MatTabsModule
+    MatTabsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
